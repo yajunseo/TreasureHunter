2275063c7956665662593b43e646a095118275ad
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-06-16 19:02:13 +0900

    [헌터/적/야준서] 프로젝트 생성

---
+++ TreasureHunter/Source/TreasureHunter/TreasureHunter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TreasureHunter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TreasureHunter, "TreasureHunter" );
