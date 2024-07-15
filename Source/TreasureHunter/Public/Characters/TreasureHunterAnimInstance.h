ec591957af7c0847eb8f49cc19e4ca3f3feaa8bf
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-07-15 23:48:28 +0900

    [헌터/애니메이션/야준서] 애님 인스턴스 생성

---
+++ Source/TreasureHunter/Public/Characters/TreasureHunterAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TreasureHunterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASUREHUNTER_API UTreasureHunterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class ATreasureHunterCharacter* TreasureHunterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* TreasureHunterMovementComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+};
