517425234f2a3ebcf519c6a8035d9f9aab0c48b7
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-07-29 22:11:22 +0900

    [헌터/애니메이션/야준서] 공격 애니메이션 추가

---
+++ Source/TreasureHunter/Public/Items/Item.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class TREASUREHUNTER_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float Amplitude = 2.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float TimeConstant = 3.0f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime = 0.0f;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
+	UPROPERTY(VisibleAnywhere)
+	class USphereComponent* Sphere;
+};
