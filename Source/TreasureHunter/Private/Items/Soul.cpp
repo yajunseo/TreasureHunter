7d84b0c6a6766ed486fe6ee90bafef3a641cd0fe
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-09-14 00:44:11 +0900

    [헌터/아이템/야준서] 영혼 바닥 까지 떨어지는 기능 추가

--- Source/TreasureHunter/Private/Items/Soul.cpp
+++ Source/TreasureHunter/Private/Items/Soul.cpp
@@ -3,7 +3,47 @@
 
 #include "Items/Soul.h"
 
+#include "Evaluation/Blending/MovieSceneBlendType.h"
 #include "Interfaces/PickUpInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void ASoul::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if(GetActorLocation().Z > DesiredZ)
+	{
+		const FVector DeltaLocation = FVector(0.f, 0.f, DriftRate * DeltaSeconds);
+		AddActorWorldOffset(DeltaLocation);
+	}
+}
+
+void ASoul::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const FVector Start = GetActorLocation();
+	const FVector End = Start - FVector(0.f, 0.f, 2000.f);
+
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(EObjectTypeQuery::ObjectTypeQuery1);
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetOwner());
+	FHitResult HitResult;
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ObjectTypes,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::None,
+		HitResult,
+		true
+	);
+
+	DesiredZ = HitResult.ImpactPoint.Z + 50.f;
+}
 
 void ASoul::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
--- Source/TreasureHunter/Public/Items/Soul.h
+++ Source/TreasureHunter/Public/Items/Soul.h
@@ -14,14 +14,22 @@ class TREASUREHUNTER_API ASoul : public AItem
 {
 	GENERATED_BODY()
 
+public:
+	virtual void Tick(float DeltaSeconds) override;
 
 protected:
+	virtual void BeginPlay() override;
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Soul Properties")
 	int32 Souls;
 
+	float DesiredZ;
+
+	UPROPERTY(EditAnywhere)
+	float DriftRate = -15.f;
+	
 public:
 	FORCEINLINE int32 GetSouls() const { return Souls; }
 	FORCEINLINE void SetSouls(int32 SoulCnt) { Souls = SoulCnt; }
